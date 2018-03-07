ce7174f1f3ce3aff49c42dc8e11590d1ebd2fa64
Caleb Mahala <31549512+calebgmahala@users.noreply.github.com>
2018-03-06 19:40:59 -0500

    This is late, I am using cake as my php framework. I am attempting to make a news app, as of now this push is what I had. I forgot to initialize a initial repo

---
+++ src/Controller/NewsController.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link      https://cakephp.org CakePHP(tm) Project
+ * @since     0.2.9
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\Network\Exception\ForbiddenException;
+use Cake\Network\Exception\NotFoundException;
+use Cake\View\Exception\MissingTemplateException;
+use Cake\Datasource\ConnectionManager;
+
+/**
+ * Static content controller
+ *
+ * This controller will render views from Template/Pages/
+ *
+ * @link https://book.cakephp.org/3.0/en/controllers/pages-controller.html
+ */
+class NewsController extends AppController
+{
+
+    /**
+     * Displays a view
+     *
+     * @param array ...$path Path segments.
+     * @return \Cake\Http\Response|null
+     * @throws \Cake\Network\Exception\ForbiddenException When a directory traversal attempt.
+     * @throws \Cake\Network\Exception\NotFoundException When the view file could not
+     *   be found or \Cake\View\Exception\MissingTemplateException in debug mode.
+     */
+    public function index()
+    {
+        $connection = ConnectionManager::get('default');
+
+
+
+        //$this->set(compact('page', 'subpage'));
+        $results = $connection->execute('SELECT * FROM articles;')->fetchAll('assoc');
+        $this->set('table', $results);
+        try {
+            $this->render( 'dashboard' );
+        } catch (MissingTemplateException $exception) {
+            if (Configure::read('debug')) {
+                throw $exception;
+            }
+            throw new NotFoundException();
+        }
+
+        //return $this->redirect('/'.$path);
+    }
+
+    public function create()
+    {
+        try {
+            $this->render( 'create' );
+        } catch (MissingTemplateException $exception) {
+            if (Configure::read('debug')) {
+                throw $exception;
+            }
+            throw new NotFoundException();
+        }
+    }
+
+    public function show($path)
+    {
+        $connection = ConnectionManager::get('default');
+
+        $this->set('article', $path);
+        $results = $connection->execute("SELECT * FROM articles WHERE id='$path' ;")->fetch('assoc');
+        $this->set('table', $results);
+        try {
+            $this->render( 'show' );
+        } catch (MissingTemplateException $exception) {
+            if (Configure::read('debug')) {
+                throw $exception;
+            }
+            throw new NotFoundException();
+        }
+    }
+
+    public function new() {
+        $connection = ConnectionManager::get('default');
+        //$result = $connection->prepare('CREATE TABLE articles (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, article TEXT NOT NULL, author TEXT NOT NULL);');
+        //$result->bind_param('i', $_GET['delpost']);
+        //$result->execute();
+        $title = $_POST["title"];
+        $text = $_POST["text"];
+        $author = 'user';
+        $connection->execute("INSERT INTO articles(title, article, author) VALUES('$title', '$text.', '$author');");
+        index();
+    }
+}
---
+++ src/Template/News/create.ctp
@@ -0,0 +1,40 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         0.10.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+use Cake\Cache\Cache;
+use Cake\Core\Configure;
+use Cake\Core\Plugin;
+use Cake\Datasource\ConnectionManager;
+use Cake\Error\Debugger;
+use Cake\Network\Exception\NotFoundException;
+
+if (!Configure::read('debug')) :
+    throw new NotFoundException(
+        'Please replace src/Template/Pages/home.ctp with your own version or re-enable debug mode.'
+    );
+endif;
+
+$cakeDescription = 'CakePHP: the rapid development PHP framework';
+?>
+
+<header class="row">
+    <div class="header-title">
+        <h1>Create an article</h1>
+    </div>
+</header>
+<form action="/news/create_article" method="post">
+<input type="text" name="title">
+<textarea name="text"></textarea>
+<input type="submit">
+</form>
